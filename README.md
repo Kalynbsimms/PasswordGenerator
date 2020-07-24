de8665dc042aa0bce8dcb04ba0d511c50578be20
Kalyn Simms <Kalynbsimms@gmail.com>
2020-07-23 21:47:43 -0700

    code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PasswordGenerator
+
+**Created several if and else statements to generate a random password with the click of the button using Javascript**
+
--- script.js
+++ script.js
@@ -1,104 +1,65 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-generateBtn.addEventListener("click", writePassword);
 
-function writePassword() {
+// Defining the variables that will be used in the conditional statements
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+let numberCase = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+let specialCase = ['+', '-', '&', '!', '(', ')', '{', '}', '[', ']', '^', '~', '*', '?', ':']
+
+let passWord = []
+
+// Password Generator scenarios
+
+const generatePassword = () => {
+
+  let possiblePassword = []
+  
+
 
   let passLength = parseInt(prompt('Between 8 - 128, how long would you want your password?'))
+
   if (passLength < 8 || passLength > 128) {
-    alert('Pick a Valid Number!')
+    alert('Please choose a valid length')
     return
   }
 
-  let upperConfirm = confirm('Would you like uppercase?')
-  let lowerConfirm = confirm('Would you like lowercase?')
-  let numberConfirm = confirm('Would you like numbers?')
-  let specialConfirm = confirm('Would you like special characters?')
+  if (confirm('Would you like to add upperCase to your Password?')) {
+    possiblePassword = possiblePassword.concat(upperCase)
+    
 
-
-
-  let characters = {
-    upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-    lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-    numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
-    special: ['+', '-', '&', '!', '(', ')', '{', '}', '[', ']', '^', '~', '*', '?', ':']
+  }
+  if (confirm('Would you like to add lowerCase to your Password?')) {
+    possiblePassword = possiblePassword.concat(lowerCase)
+  
+  }
+  if (confirm('Would you like to numbers to your Password?')) {
+    possiblePassword = possiblePassword.concat(numberCase)
+   
   }
 
-
-
-
-  let password = ''
+  if (confirm('Would you like to add special characters to your Password?')) {
+    possiblePassword = possiblePassword.concat(specialCase)
+    
+  }
 
   for (let i = 0; i < passLength; i++) {
-
-    let pick = Math.floor(Math.random() * 4)
-
-    if (pick === 0) {
-
-      if (upperConfirm === true) {
-        let change = characters.upperCase[Math.floor() * characters.upperCase.length]
-        password = password + change
-      } else {
-        i--
-      }
-
-    } else if (pick === 1) {
-      if (lowerConfirm === true) {
-        let change = characters.lowerCase[Math.floor() * characters.lowerCase.length]
-        password = password + change
-      } else {
-        i--
-      }
-
-
-    } else if (pick === 2) {
-
-      if (numberConfirm === true) {
-        let change = characters.numbers[Math.floor() * characters.numbers.length]
-        password = password + change
-      } else {
-        i--
-      }
-
-    } else {
-      if (specialConfirm === true) {
-        let change = characters.special[Math.floor() * characters.special.length]
-        password = password + change
-      } else {
-        i--
-      }
-
-    }
-
-
-
-
-
+    passWord.push(possiblePassword[Math.floor(Math.random() * possiblePassword.length)])
   }
 
-
-
-
-
+ 
+// stops the function from  continuing
+  return passWord.join("");
 }
 
+// Outside of the conditional statements. generates password
+document.getElementById('generate').addEventListener('click', () => {
+  let favorites = generatePassword()
+
+  document.getElementById('password').textContent = passWord
+})
 
 
 
-
-
-
-
-
-
-
-
-// Add event listener to generate button
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
